a0ddd85237f75daa4d510f2f46254f06287c8334
rparak <59449320+rparak@users.noreply.github.com>
2022-09-10 13:57:36 +0100

    first commit

--- src/Cpp/lib.cpp
+++ src/Cpp/lib.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <cstdio>
 
+// https://peaku.co/questions/22798-pasar-un-vector-matriz-de-c%2B%2B-no-administrado-a-c%23
 // g++ -shared -fPIC -o libhello.so hello.cpp
 /*
 extern "C" int Add(int a, int b) {
@@ -9,6 +10,7 @@ extern "C" int Add(int a, int b) {
 }
 */
 
+/*
 extern "C" void Array_Mult(int nElements, int *pElements) { 
 	int x;
 	for (int i = 0; i < nElements; i++)
@@ -16,4 +18,18 @@ extern "C" void Array_Mult(int nElements, int *pElements) {
 		x = pElements[i];
 		pElements[i] = x + 10;
 	}
+}
+*/
+
+struct ArrayStruct
+{
+	std::vector<int> arr;
+	int length;
+};
+
+extern "C" __declspec(dllexport) void GetArray(ArrayStruct* a)
+{
+	a->length = 5;
+	for(int i=0; i<a->length; i++)
+		a->arr.push_back(i);
 }
--- src/Csharp/app/Program.cs
+++ src/Csharp/app/Program.cs
@@ -3,18 +3,38 @@ using System.Collections.Generic;
 using System.Runtime.InteropServices;
 
 // https://travis.media/how-to-run-csharp-in-vscode/
+// https://www.796t.com/post/YWFjcXc=.html
 namespace app
 {
     class Program
     {
-        [DllImport("lib.dll", EntryPoint = "Array_Mult", ExactSpelling = false, CallingConvention = CallingConvention.Cdecl)]
-        public static extern void Array_Mult(int a, int[] b);
+        [StructLayout(LayoutKind.Sequential)]
+        public struct ArrayStruct
+        {
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 5)]
+            public int[] arr;
+            public int length;
+        }
+
+        [DllImport("lib.dll", EntryPoint = "GetArray", ExactSpelling = false, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void GetArray(ref ArrayStruct a);
 
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             //Console.WriteLine(Add(5, 6));
 
+
+            ArrayStruct a = new ArrayStruct();
+            a.arr = new int[5];
+            GetArray(ref a);
+
+            Console.WriteLine(a.length);
+            foreach( int x in a.arr){
+                Console.WriteLine("Element is {0}", x);
+            }
+
+            /*
             int[] input_1 = { 1, 2, 3, 4, 5 };
             Array_Mult(5, input_1);
 
@@ -22,6 +42,7 @@ namespace app
             {
                 Console.WriteLine("Element is {0}", x);
             }
+            */
         }
     }
 }
