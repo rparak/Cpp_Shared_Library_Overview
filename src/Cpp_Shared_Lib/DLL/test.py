9ea3bf1e0859c8137ad42d061b12b0c1bbe4bec8
rparak <59449320+rparak@users.noreply.github.com>
2022-09-23 13:44:02 +0100

    improve dll

--- src/Cpp_Shared_Lib/DLL/test.py
+++ src/Cpp_Shared_Lib/DLL/test.py
@@ -1,24 +1,42 @@
 import ctypes as ct
 from numpy.ctypeslib import ndpointer
 
-class Find_Min_Value_Output_str(ct.Structure):
-    _fields_ = [("value", ct.c_int), ("idx", ct.c_int)]
+class FCE_ARRAY_MinMax_INPUT_Str(ct.Structure):
+    _fields_ = [('Array', ct.POINTER(ct.c_int)), ('N', ct.c_size_t)]
 
-class Find_Min_Value_Input_str(ct.Structure):
-    _fields_ = [("x", ct.POINTER(ct.c_int)), ("N", ct.c_size_t)]
+class FCE_ARRAY_MinMax_OUTPUT_Str(ct.Structure):
+    _fields_ = [('Value', ct.c_int), ('Index', ct.c_int)]
 
 __dll_lib = ct.cdll.LoadLibrary('Example_Lib_Shared.dll')
 
-
-__dll_lib.Min_Int.argtypes = [ct.POINTER(Find_Min_Value_Input_str)]
-__dll_lib.Min_Int.restype  = Find_Min_Value_Output_str
+__dll_lib.Array_MinMax.argtypes = [ct.POINTER(FCE_ARRAY_MinMax_INPUT_Str), ct.c_bool]
+__dll_lib.Array_MinMax.restype  = FCE_ARRAY_MinMax_OUTPUT_Str
 
 data = [5, 11, 3, 8, 5]
-input_v = Find_Min_Value_Input_str((ct.c_int * len(data))(*data), ct.c_size_t(len(data)))
+input_v = FCE_ARRAY_MinMax_INPUT_Str((ct.c_int * len(data))(*data), ct.c_size_t(len(data)))
 
-res = __dll_lib.Min_Int(input_v)
+res = __dll_lib.Array_MinMax(input_v, False)
 
-print(res.value, res.idx)
+print(res.Value, res.Index)
+
+__dll_lib.Generate_Random_Array.argtypes = [ct.c_int, ct.c_int, ct.c_size_t]
+__dll_lib.Generate_Random_Array.restype = ct.POINTER(ct.c_int)
+
+res_0 = __dll_lib.Generate_Random_Array(1, 1000, 10)
+
+print(type(res_0))
+for i in range(0, 10):
+    print(res_0[i])
+
+__dll_lib.Multiply_Array_By_Number.argtypes = [ct.c_int, ct.POINTER(ct.c_int), ct.c_size_t]
+__dll_lib.Multiply_Array_By_Number.restype = None
+
+py_values = [1, 2, 3, 4, 5]
+arr = (ct.c_int * len(py_values))(*py_values)
+__dll_lib.Multiply_Array_By_Number(ct.c_int(5), arr, ct.c_size_t(len(arr)))
+
+for i in range(0, len(arr)):
+    print(arr[i])
 
 """
 __dll_lib.Max_Int_Py.argtypes = (ct.POINTER(ct.c_int), ct.c_size_t)
--- src/Cpp_Shared_Lib/Example_Lib_Shared.cpp
+++ src/Cpp_Shared_Lib/Example_Lib_Shared.cpp
@@ -6,30 +6,64 @@
 // probably use only struct for in/out
 
 template <typename T> 
-struct Find_Min_Value_Output_str{
-    T value;
-    int idx;
-};
-
-template <typename T> 
-struct Find_Min_Value_Input_str{
-    T *x;
+struct FCE_ARRAY_MinMax_INPUT_Str{
+    T *Array;
     size_t N;
 };
 
+template <typename T> 
+struct FCE_ARRAY_MinMax_OUTPUT_Str{
+    T Value;
+    int Index;
+};
+
+
 extern "C" { 
-    __declspec(dllexport) Find_Min_Value_Output_str<int> Min_Int(Find_Min_Value_Input_str<int>* x)
+    __declspec(dllexport) int* Generate_Random_Array(const int MIN, const int MAX, const size_t N)
     {
-        Find_Min_Value_Output_str<int> output;
-        output.value = x->x[0]; output.idx = 0;
-        for(auto i = 1; i < x->N; ++i){
-            if(output.value > x->x[i]){
-                output.value = x->x[i];
-                output.idx   = i;
+        std::srand(time(0)); 
+
+        int* Output = new int[N];
+        for(auto i = 0; i < N; ++i){
+            Output[i] = static_cast<int>(MIN + rand() % (MAX - MIN + 1));
+        }
+
+        return Output;
+    }
+
+     __declspec(dllexport) void Multiply_Array_By_Number(const int Number, int *Array, const size_t N) 
+     { 
+        /*
+        Multiplying an array by a number
+        Multiply each element in a list by a number in
+        */
+        int x_temp;
+        for (auto i = 0; i < N; ++i)
+        {
+            x_temp = Array[i];
+            Array[i] = x_temp * Number;
+        }
+    }
+
+    __declspec(dllexport) FCE_ARRAY_MinMax_OUTPUT_Str<int> Array_MinMax(FCE_ARRAY_MinMax_INPUT_Str<int>* Input, const bool return_min_val)
+    {
+        FCE_ARRAY_MinMax_OUTPUT_Str<int> Output;
+        Output.Value = Input->Array[0]; Output.Index = 0;
+        for(auto i = 1; i < Input->N; ++i){
+            if(return_min_val == true){
+                if(Output.Value > Input->Array[i]){
+                    Output.Value = Input->Array[i];
+                    Output.Index = i;
+                }
+            }else{
+                if(Output.Value < Input->Array[i]){
+                    Output.Value = Input->Array[i];
+                    Output.Index = i;
+                }
             }
         }
 
-       return output;
+       return Output;
     }
 
     /*
