61269343d763a052983b05dbf66294d49d480c17
rparak <59449320+rparak@users.noreply.github.com>
2022-09-23 09:40:02 +0100

    improve dll

--- src/Cpp_Shared_Lib/DLL/test.py
+++ src/Cpp_Shared_Lib/DLL/test.py
@@ -1,8 +1,26 @@
 import ctypes as ct
 from numpy.ctypeslib import ndpointer
 
+class Find_Min_Value_Output_str(ct.Structure):
+    _fields_ = [("value", ct.c_int), ("idx", ct.c_int)]
+
+class Find_Min_Value_Input_str(ct.Structure):
+    _fields_ = [("x", ct.POINTER(ct.c_int)), ("N", ct.c_size_t)]
+
 __dll_lib = ct.cdll.LoadLibrary('Example_Lib_Shared.dll')
 
+
+__dll_lib.Min_Int.argtypes = [ct.POINTER(Find_Min_Value_Input_str)]
+__dll_lib.Min_Int.restype  = Find_Min_Value_Output_str
+
+data = [5, 11, 3, 8, 5]
+input_v = Find_Min_Value_Input_str((ct.c_int * len(data))(*data), ct.c_size_t(len(data)))
+
+res = __dll_lib.Min_Int(input_v)
+
+print(res.value, res.idx)
+
+"""
 __dll_lib.Max_Int_Py.argtypes = (ct.POINTER(ct.c_int), ct.c_size_t)
 __dll_lib.Max_Int_Py.restype  = ndpointer(dtype=ct.c_int, shape=(2,))
 
@@ -11,6 +29,7 @@ data_ct = (ct.c_int * len(data))(*data)
 res = __dll_lib.Max_Int_Py(data_ct, ct.c_size_t(5))
 
 print(res[0])
+"""
 
 
 
--- src/Cpp_Shared_Lib/Example_Lib_Shared.cpp
+++ src/Cpp_Shared_Lib/Example_Lib_Shared.cpp
@@ -4,7 +4,35 @@
 // g++ -shared -std=c++17 -fPIC -o SO/Example_Lib_Shared.so Example_Lib_Shared.hpp
 
 // probably use only struct for in/out
+
+template <typename T> 
+struct Find_Min_Value_Output_str{
+    T value;
+    int idx;
+};
+
+template <typename T> 
+struct Find_Min_Value_Input_str{
+    T *x;
+    size_t N;
+};
+
 extern "C" { 
+    __declspec(dllexport) Find_Min_Value_Output_str<int> Min_Int(Find_Min_Value_Input_str<int>* x)
+    {
+        Find_Min_Value_Output_str<int> output;
+        output.value = x->x[0]; output.idx = 0;
+        for(auto i = 1; i < x->N; ++i){
+            if(output.value > x->x[i]){
+                output.value = x->x[i];
+                output.idx   = i;
+            }
+        }
+
+       return output;
+    }
+
+    /*
     __declspec(dllexport) std::tuple<int, int> Max_Int(const int *x, const size_t N)
     {   
         std::vector<int> x_v(x, x + N);
@@ -34,4 +62,5 @@ extern "C" {
     __declspec(dllexport) Example_Lib::Find_Min_Value_Output_str<double> Min_Double(Example_Lib::Find_Min_Value_Input_str<double> *x){
         return Example_Lib::Min<double>(x);
     }
+    */
 }
